4cc05a25fd1dcc3204564811f9a8fcbda96176af
Randallzoeng <randallzoeng@qq.com>
2018-06-29 16:06:38 +0800

    :beers: drunkenly coding

--- DataCastle Competition for Brutal Age.R
+++ DataCastle Competition for Brutal Age.R
@@ -7,6 +7,9 @@ train<- read.table("tap_fun_train.csv",sep=",",
 row.names(train) <- train$user_id
 train$lifetime <- as.numeric(as.Date(max(train$register_time))-as.Date(train$register_time))
 
+test<- read.table("tap_fun_test.csv",sep=",",
+                   header=TRUE, fileEncoding="UTF-8-BOM",
+                   stringsAsFactors=FALSE,fill=TRUE)
 
 #2. prediction using simple liner regression (frame problems)
 train_simp <- train[,3:110]
@@ -19,7 +22,7 @@ summary(fit)
 library(Metrics)
 rmse(train_simp$prediction_pay_price,fit$fitted.values)
 
-RMSE(pred = fit$fitted.values,obs = train_simp$prediction_pay_price)
+rmse(pred = fit$fitted.values,obs = train_simp$prediction_pay_price)
 #[1] 57.02891
 #As the measure Metrics of this competition is RMSE, 
 #so I must figure out a model whose RMSE is less than this simple regression
@@ -58,13 +61,117 @@ length(setdiff(list_nopay45d,list_nopayweek))
 length(setdiff(list_nopayweek,list_nopay45d))
 #4549 made purchse in the 45d even they pay nothing in 1st week, what dirves it?
 
-#Vis it using waffle
+#Vis it using waffle or we could use venn from gplots
 library(waffle)
 users<-c('Made purchases in 1st week'=41439/3000,'No purchases in next 45d'=2242019/3000,'Made purchases after 1st week'=4549/3000)
 waffle(users,xlab='',legend_pos = "top")
-#Vis it using venn
-# library(gplots)
-# venn(list(list_allusers,list_nopayweek,list_nopay45d))
+
+#now let's see how price distribut
+
+summary(train[train$pay_price!=0,'pay_price'])
+summary(train[train$prediction_pay_price!=0,'pay_price'])
+
+
+pay_price <- train$pay_price
+prediction_pay_price <- train$prediction_pay_price
+
+hist(prediction_pay_price,             
+     freq=FALSE,                                
+     col="pink",                                     
+     xlab="prediction_pay_price",                       
+     main="distribution of prediction_pay_price")     
+rug(jitter(prediction_pay_price))                             
+lines(density(prediction_pay_price), col="navy", lwd=2)   
+
+prediction_pay_price <- NULL
+
+#xgboost
+library(xgboost)
+
+dataset <- as.matrix(train[,3:109])
+
+bst <- xgboost(data = dataset, label = dataset[,107], max_depth = 2, eta = 1,
+               nrounds = 2, objective = "reg:linear")
+
+xgb.save(bst, 'model.save')
+bst = xgb.load('model.save')
+pred <- predict(bst, test$data)
+
+library(Matrix)
+Matrix(as.matrix(x), sparse = TRUE)
+
+
+####features combine
+
+df <- data.frame()
+df <- train['lifetime']
+df$wood <- train$wood_add_value + train$wood_reduce_value
+df$stone <- train$stone_add_value - train$stone_reduce_value
+df$ivory <- train$ivory_add_value - train$ivory_reduce_value
+df$meat <- train$meat_add_value - train$meat_reduce_value
+df$magic <- train$magic_add_value - train$magic_reduce_value
+df$infantry <- train$infantry_add_value - train$infantry_reduce_value - train$wound_infantry_add_value + train$wound_infantry_reduce_value
+df$cavalry <- train$cavalry_add_value - train$cavalry_reduce_value - train$wound_cavalry_add_value + train$wound_cavalry_reduce_value
+df$shaman <- train$shaman_add_value - train$shaman_reduce_value - train$wound_shaman_add_value + train$wound_shaman_reduce_value
+#acc
+df$gen_acc <- train$general_acceleration_add_value - train$general_acceleration_reduce_value
+df$bld_acc <- train$building_acceleration_add_value - train$building_acceleration_reduce_value
+df$rsh_acc <- train$reaserch_acceleration_add_value - train$reaserch_acceleration_reduce_value
+df$train_acc <- train$training_acceleration_add_value - train$training_acceleration_reduce_value
+df$treat_acc <- train$treatment_acceleraion_add_value - train$treatment_acceleration_reduce_value
+
+#level
+df$bdlevel <- apply(train[,35:50],1,sum)
+df$sclevel <- apply(train[,51:99],1,sum)
+#other info
+df <- cbind(df,train[,100:109])
+
+
+#what about now?
+fit <- lm(prediction_pay_price~.,data=df)
+library(Metrics)
+rmse(actual = df$prediction_pay_price,predicted = fit$fitted.values)
+#unfortunately it rised to 58.8306
+df_pay <- df[df$pay_price!=0,]
+df_nopay <- df[df$pay_price==0,]
+
+fit_pay <- lm(prediction_pay_price~.,data=df_pay)
+fit_nopay <- lm(prediction_pay_price~.,data=df_nopay)
+
+rmse(actual = df_pay$prediction_pay_price,predicted = fit_pay$fitted.values)
+rmse(actual = df_nopay$prediction_pay_price,predicted = fit_nopay$fitted.values)
+
+##let's using xgboost
+
+library(xgboost)
+
+dataset <- as.matrix(train[,3:109])
+dataset_pay <- as.matrix(train[train$pay_price!=0,3:109])
+
+bst <- xgboost(data = dataset_pay, label = dataset_pay[,107], max_depth = 2, eta = 1,
+               nrounds = 5, objective = "reg:linear")
+
+# [1]	train-rmse:224.188126 
+# [2]	train-rmse:188.529678 
+# [3]	train-rmse:153.518906 
+# [4]	train-rmse:140.240799 
+# [5]	train-rmse:112.550278 
+
+xgb.save(bst, 'model.save')
+bst = xgb.load('model.save')
+
+pred <- predict(bst, as.matrix(test[,3:108]))
+test$prediction_pay_price <- pred
+row.names(test)<- test$user_id
+write.csv(x=test[,c(1,109)],file = "upload.csv")
+
+
+#library(Matrix)
+#Matrix(as.matrix(x), sparse = TRUE)
+
+
+
+
 
 
 
